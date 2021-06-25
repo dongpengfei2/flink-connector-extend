e97dfe0c7ce58241aa3aae50bcbbd7597a258fae
dongpengfei1 <dongpengfei1@shihuituan.com>
2021-06-25 13:03:11 +0800

    first commit

---
+++ flink-connector-clickhouse/src/test/java/com/sht/connector/clickhouse/TestClickHouseConnector.java
@@ -0,0 +1,39 @@
+package com.sht.connector.clickhouse;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+public class TestClickHouseConnector {
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamTableEnvironment tableEnvironment = StreamTableEnvironment.create(env);
+        tableEnvironment.executeSql(" " +
+            " CREATE TABLE sourceTable ( " +
+            "  a varchar, " +
+            "  b varchar " +
+            " ) WITH ( " +
+            "  'connector.type' = 'kafka', " +
+            "  'connector.version' = 'universal', " +
+            "  'connector.topic' = 'flink_source', " +
+            "  'connector.startup-mode' = 'earliest-offset', " +
+            "  'connector.properties.zookeeper.connect' = '127.0.0.1:2181', " +
+            "  'connector.properties.bootstrap.servers' = '127.0.0.1:9092', " +
+            "  'format.type' = 'json' " +
+            " ) ");
+        tableEnvironment.executeSql("CREATE TABLE sinkTable (" +
+            "    a String," +
+            "    b String" +
+            ") WITH (" +
+            "    'connector' = 'clickhouse'," +
+            "    'url' = 'jdbc:clickhouse://127.0.0.1:8123/nicetuan_webapp'," +
+            "    'table-name' = 'dwd_user_login'," +
+            "    'username' = 'default'," +
+            "    'password' = ''," +
+            "    'format' = 'json'" +
+            ")");
+        tableEnvironment.executeSql(
+            "insert into sinkTable " +
+                "select * " +
+                "from sourceTable");
+    }
+}
